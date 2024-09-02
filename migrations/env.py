a1434eaf1e15c23664a6f9dc0e534192dc4254b1
ustsl <w@imvo.site>
2024-09-02 20:49:04 +0300

    update dashboard model

--- migrations/env.py
+++ migrations/env.py
@@ -20,7 +20,11 @@ from src.database.models.account_model.tables import AccountModel
 from src.database.models.data_model.tables import DataModel
 from src.database.models.widget_model.tables import WidgetModel
 from src.database.models.chart_model.tables import ChartModel
-from src.database.models.dashboard_model.tables import DashboardModel
+from src.database.models.dashboard_model.tables import (
+    DashboardModel,
+    DashboardChart,
+    DashboardWidget,
+)
 
 target_metadata = Base.metadata
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@localhost:5432/{DB_NAME}"
--- src/database/models/dashboard_model/tables.py
+++ src/database/models/dashboard_model/tables.py
@@ -6,10 +6,41 @@ from sqlalchemy.dialects.postgresql import UUID
 from src.database.mixins.base import TimeModel
 from src.database.base import Base
 
+from sqlalchemy.orm import relationship
+
+
+class DashboardChart(Base):
+    __tablename__ = "dashboard_chart"
+
+    dashboard_id = Column(
+        UUID(as_uuid=True), ForeignKey("dashboard.id"), primary_key=True
+    )
+    chart_id = Column(UUID(as_uuid=True), ForeignKey("chart.id"), primary_key=True)
+
+    dashboard = relationship("DashboardModel", back_populates="charts")
+
+
+class DashboardWidget(Base):
+    __tablename__ = "dashboard_widget"
+
+    dashboard_id = Column(
+        UUID(as_uuid=True), ForeignKey("dashboard.id"), primary_key=True
+    )
+    widget_id = Column(UUID(as_uuid=True), ForeignKey("widget.id"), primary_key=True)
+
+    dashboard = relationship("DashboardModel", back_populates="widgets")
+
 
 class DashboardModel(Base, TimeModel):
     __tablename__ = "dashboard"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     title = Column(String, nullable=False)
-    items = Column(JSON, nullable=False)
+    settings = Column(JSON, nullable=False)
+
+    charts = relationship(
+        "DashboardChart", back_populates="dashboard", cascade="all, delete-orphan"
+    )
+    widgets = relationship(
+        "DashboardWidget", back_populates="dashboard", cascade="all, delete-orphan"
+    )
