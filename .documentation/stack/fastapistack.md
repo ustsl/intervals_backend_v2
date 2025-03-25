1e3c3e7930af6ec43fac968fa3435d73281fe89c
ustsl <w@imvo.site>
2025-03-25 13:20:56 +0300

    upd

--- .documentation/stack/fastapistack.md
+++
@@ -1,2 +0,0 @@
-1. fastapi
-2. websockets
--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from src.api.routes import main_api_router
 #########################
 
 # create instance of the app
-app = FastAPI(title="INTERVALS BACK-END. BY USTSL.RU")
+app = FastAPI(title="INTERVALS API")
 
 
 app.add_middleware(
